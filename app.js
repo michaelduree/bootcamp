929000530a25d6227d816b800e36476e4b551169
Michael DuRee <michael.duree@t-mobile.com>
2021-06-15 12:51:02 -0400

    div

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.listen(port, () => {
